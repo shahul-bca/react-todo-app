950c1660cc172f3c033b5004d2797aff11af33e4
shahul-bca <pshahul913@gmail.com>
2024-11-26 15:35:08 +0530

    UI designing

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,11 +1,24 @@
-import React from 'react'
-import './style.css'
+import React from "react";
+import "./style.css";
 const Todo = () => {
   return (
-    <div>
-      <h2>TODO-APP</h2>
+    <div className="container">
+      <h1>To Do List</h1>
+      <div className="add-box">
+        <input type="text" placeholder="Enter Your Taskset" />
+        <button id="add-btn">ADD</button>
+      </div>
+      <h3>Task List</h3>
+      <ul>
+        <li>
+          task1<i className="fa fa-trash" aria-hidden="true"></i>
+        </li>
+        <li>
+          task2<i className="fa fa-trash" aria-hidden="true"></i>
+        </li>
+      </ul>
     </div>
-  )
-}
+  );
+};
 
-export default Todo
+export default Todo;
