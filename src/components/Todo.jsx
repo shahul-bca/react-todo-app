b35d27c995b7984870bbca6fba84500d4d829f5a
shahul-bca <pshahul913@gmail.com>
2024-11-27 11:53:28 +0530

    Delete functionality added successfully.

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,13 +1,22 @@
 import React, { useState } from "react";
 import "./style.css";
+import { IoCheckmarkDoneCircle } from "react-icons/io5";
 const Todo = () => {
   const [todo, setTodo] = useState("");
   const [todos, setTodos] = useState([]);
 
+  //add function
   const addTodo = () => {
-    setTodos([...todos, todo]);
+    setTodos([...todos, { list: todo, id: Date.now() }]);
     console.log(todo);
+    setTodo("");
   };
+
+  //delete function
+  const onDelete = (id) => {
+    setTodos(todos.filter((todo) => todo.id !== id));
+  };
+
   return (
     <div className="container">
       <h1>To Do List</h1>
@@ -26,7 +35,17 @@ const Todo = () => {
       <ul>
         <ul>
           {todos.map((todo, index) => (
-            <li key={index}>{todo}<i className="fa fa-trash" aria-hidden="true"></i></li>
+            <li key={index}>
+              {todo.list}
+              <i
+                className="fa fa-trash"
+                aria-hidden="true"
+                onClick={() => onDelete(todo.id)}
+              >
+                <button>EDIT</button>
+              </i>
+              <IoCheckmarkDoneCircle id="don" />
+            </li>
           ))}
         </ul>
       </ul>
